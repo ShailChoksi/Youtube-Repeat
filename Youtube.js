c51d71e3597c65c4a3e2e77d95fd938c9316421d
Shail Choksi <sschoksi@uwaterloo.ca>
2014-06-26 13:11:01 -0400

    Added event listener for page title change. Fixed history.pushState bug

--- Youtube.js
+++ Youtube.js
@@ -4,3 +4,17 @@ s.onload = function() {
     this.parentNode.removeChild(this);
 };
 (document.head||document.documentElement).appendChild(s);
+
+
+document.addEventListener("DOMTitleChanged", "InjectScript");
+
+function InjectScript()
+{
+  var s = document.createElement('script');
+  s.src = chrome.extension.getURL('Script.js');
+  s.onload = function() {
+      this.parentNode.removeChild(this);
+  };
+  (document.head||document.documentElement).appendChild(s);
+
+}
